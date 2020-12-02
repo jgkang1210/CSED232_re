c69f6b2e1f89efd636ac42c3c9dd9d2da2628d30
jgkang1210 <55006435+jgkang1210@users.noreply.github.com>
2020-12-02 22:56:50 +0900

    2nd

--- ASSN4/Pokedex.h
+++ ASSN4/Pokedex.h
@@ -1,5 +1,26 @@
-#pragma once
+#ifndef POKEDEX
+#define POKEDEX
+
+#include <iostream>
+#include "Pokemon.h"
+#include <string>
+#include <vector>
+
 class Pokedex
 {
+private:
+	std::vector<Pokemon*> pokedex;
+
+public:
+	Pokedex();
+	~Pokedex();
+	void capturePokemon(const Pokemon* Pokemon);
+	void depositPokemon(const int& index);
+	const Pokemon* watchPokemon(const int& index) const;
+	void printByDefaullt() const;
+	void printByName() const;
+	void printById() const;
+	void printBySearch(std::string name);
 };
 
+#endif
--- ASSN4/Pokemon.cpp
+++ ASSN4/Pokemon.cpp
@@ -1,2 +1,47 @@
 
 #include "Pokemon.h"
+
+Pokemon::Pokemon()
+{
+	name = "";
+	index = 0;
+	capture_rate = 0;
+}
+
+Pokemon::Pokemon(std::string name_, int index_, int capture_rate_):name(name_), index(index_), capture_rate(capture_rate_)
+{
+}
+
+Pokemon::~Pokemon()
+{
+}
+
+std::string Pokemon::get_name() const
+{
+	return name;
+}
+
+void Pokemon::set_name(const std::string& name_)
+{
+	name = name_;
+}
+
+int Pokemon::get_index() const
+{
+	return index;
+}
+
+void Pokemon::set_index(const int& index_)
+{
+	index = index_;
+}
+
+int Pokemon::get_capture_rate() const
+{
+	return capture_rate;
+}
+
+void Pokemon::set_capture_rate(const int& capture_rate_)
+{
+	capture_rate = capture_rate_;
+}
