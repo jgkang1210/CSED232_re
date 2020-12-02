65f070d2e48cfd49530c243b688ae099c77f02ff
Jungill Kang <jgkang1210@postech.ac.kr>
2020-12-02 23:21:43 +0900

    resolve

--- ASSN4/Pokedex.h
+++ ASSN4/Pokedex.h
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 #ifndef POKEDEX
 #define POKEDEX
 
+=======
+#pragma once
+>>>>>>> parent of c69f6b2... 2nd
 class Pokedex
 {
 };
 
+<<<<<<< HEAD
 #endif
 
+=======
+>>>>>>> parent of c69f6b2... 2nd
--- ASSN4/Pokemon.h
+++ ASSN4/Pokemon.h
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 #ifndef POKEMON
 #define POKEMON
+=======
+>>>>>>> parent of c69f6b2... 2nd
 
 class Pokemon
 {
 };
+<<<<<<< HEAD
 
 #endif POKEMON
+=======
+>>>>>>> parent of c69f6b2... 2nd
